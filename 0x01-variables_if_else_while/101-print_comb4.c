bc28c2cb23df516007d5544c6d4bcc570a0263a6
Momoh38 <momohabdulsalam38@gmail.com>
2022-12-12 06:56:18 -0800

    test

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*comment goes here
+*/
+int main(void)
+{
+	int num1,num2,num3;
+	
+	if (num1 = 10 && num2 <= 10 && num3 <= 10 )
+	{ 
+		putchar ('0' + num1);
+		putchar ('0' + num2);
+		putchar (' ');
+	}num1++ num2++ num3++
+	putchar ('\n');
+	return (0);	
+}
