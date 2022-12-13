95f70c322a754803e393d832644645f6dc0c6788
Momoh38 <momohabdulsalam38@gmail.com>
2022-12-13 09:25:25 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_alpabets to small letters
- * Return:0
+ * print_alpabet - entry point
+ * Return: 0
  */
 void print_alphabet(void)
 {
 	char m = 'a';
 
-	while (m <= "z")
+	while (m = "z")
 	{
 		_putchar(m);
 		m++;
