571ac0a948cc3e25847cba0022127965c9ba46b4
Momoh38 <momohabdulsalam38@gmail.com>
2022-12-13 09:14:37 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alpabets to small letters
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char m = 'a';
+
+	while (d <= "z")
+	{
+		_putchar(m);
+		m++;
+	}
+
+	_putchar('\n');
+}
