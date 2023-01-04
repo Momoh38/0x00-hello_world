405cf43c76b31816d824ad44e9a37a00c7f63613
Momoh38 <momohabdulsalam38@gmail.com>
2023-01-04 09:40:56 +0100

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *_puts_recursion - putchar function
+ *@s: input
+ *Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	char s;
+	_putchar("This is a string \n", s);
+	return 0;
+
+
+}
